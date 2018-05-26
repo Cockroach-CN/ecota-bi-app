993b27e053ce1576f97bd29a1f992440dcb13e2d
zhangxl <zhangxl@sqbj.com>
2018-05-26 21:25:56 +0800

    修改fn方法

--- src/services/service.js
+++ src/services/service.js
@@ -6,7 +6,7 @@ const getTabData = async (tab, opts) => {
     var params = {};
     var values = Object.keys(opts).map(key => opts[key]).filter(o => o.class === "list");
     (values || []).map(v => params[v.key] = v.value);
-    // console.log(tab.key, params);
+    console.log(tab.key, params);
 
     const datas = JSON.parse(JSON.stringify(tab.lines || []));
     return new Promise(function (resolve, reject) {
@@ -25,6 +25,9 @@ const getTabData = async (tab, opts) => {
             }));
             resolve(datas);
         });
+    }).then(r => {
+        console.log(r);
+        return r;
     });
 }
 
@@ -32,10 +35,12 @@ const getChartData = (key, opts) => {
     var params = {};
     var values = Object.keys(opts).map(key => opts[key]).filter(o => o.class === "info");
     (values || []).map(v => params[v.key] = v.value);
-    // console.log(key, params);
-    const data = get_sense_data(key, params);
-    console.log(data);
-    return data;
+    console.log(key, params);
+
+    return get_sense_data(key, params).then(r => {
+        console.log(r);
+        return r;
+    });
 }
 
 export {
